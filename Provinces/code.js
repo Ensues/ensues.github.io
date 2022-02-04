3f7d3c8d3722e04096aac2736426fdef4711de53
lolguy416859 <lolguy416859@gmail.com>
2022-02-04 13:23:11 -0600

    Update code.js

--- Provinces/code.js
+++ Provinces/code.js
@@ -1,4 +1,8 @@
 function attemptCreateRoom() {
+    roomCode = "";
+    for (let i = 0; i < 8; i++) {
+        roomCode += String.fromCharCode(getRandomInt(65,90));
+    }
     roomCode = "0";
     console.log("Attempted to create room with code " + roomCode);
 }
@@ -7,3 +11,10 @@ function attemptJoinRoom() {
     roomCode = "1";
     console.log("Attempted to join room with code " + roomCode);
 }
+
+
+function validCode(code) {
+    return true;
+}
+
+getRandomInt = (min,max) => Math.floor(Math.random() * (max-min+1))+min;
