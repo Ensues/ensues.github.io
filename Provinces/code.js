7f7408babeb7b3fcfa2b93cfd8c5894c4551deed
lolguy416859 <lolguy416859@gmail.com>
2022-02-04 12:46:33 -0600

    Created buttons in Provinces

---
+++ Provinces/code.js
@@ -0,0 +1,9 @@
+function attemptCreateRoom() {
+    roomCode = "0";
+    console.log("Attempted to create room with code " + roomCode);
+}
+
+function attemptJoinRoom() {
+    roomCode = "1";
+    console.log("Attempted to join room with code " + roomCode);
+}
