952b53f3733c0400b92a37b272874e6f818e4fdb
lolguy416859 <lolguy416859@gmail.com>
2022-02-26 12:09:45 -0600

    setup editor + textures

---
+++ TwentyFour/lib/room.ts
@@ -0,0 +1,36 @@
+class Room {
+    dimX: number;
+    dimY: number;
+    tiles: string[];
+    tileset: TileSet;
+
+    constructor(dimX: number, dimY: number, dat: string[], tileset: TileSet) {
+        this.dimX = dimX;
+        this.dimY = dimY;
+        this.tiles = dat;
+        this.tileset=tileset;
+    }
+}
+
+class TileType {
+    safeName: string;
+    walkable: boolean;
+
+    constructor(name: string, walkable: boolean = true) {
+        this.safeName = name;
+        this.walkable = walkable;
+    }
+}
+
+class TileSet {
+    data: object = {};
+    
+    set(name: string, val: TileType) {
+        this.data[name] = val;
+    }
+    get(name: string): TileType {
+        return this[name];
+    }
+}
+
+export { Room, TileSet, TileType };
